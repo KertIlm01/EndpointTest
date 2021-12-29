a6c61d66d5f77fb8fdf09c6e4d2883054d7e9229
Kert <kertilm@hotmail.com>
2021-12-29 13:33:09 +0200

    Errored log files

--- calculate_test.py
+++ calculate_test.py
@@ -3,17 +3,20 @@ import json
 import re
 import time
 import calendar
+import os
 
 calculate1_results = {"totalRepayableAmount":202052.04,"monthlyPayment":2886.45,"apr":11.4}
-calculate2_results = {"totalRepayableAmount":11032.47,"monthlyPayment":919.38,"apr":38.46}
+calculate2_results = {"totalRepayableAmount":11032.48,"monthlyPayment":919.38,"apr":38.46}
 
 def init():
+    global failed
     global logFileName
     global startTime
     startTime = str(calendar.timegm(time.gmtime()))
     print("Start time: ",startTime)
     logFileName = "logFile_"+startTime+".log"
     print(logFileName)
+    failed = "false"
 
 def log(text):
     logFile = open(logFileName,"a")
@@ -24,10 +27,12 @@ def log(text):
 
 def verifyResults(result,desiredResult):
     desiredjson = json.dumps(desiredResult,separators=(',', ':'))
+    global failed
     if result == str(desiredjson):
         log("Test : PASS")
         print("Pass")
     else:
+        failed = "true"
         log("FAIL : \n"+ "Result not matching\n"+ "desired result:\n"+str(desiredjson)+"Actual result:\n"+str(result) )
         print("desired: ")
         print(str(desiredjson))
@@ -49,7 +54,12 @@ def testCalculation(length,amount,interest,desiredResults):
     log("running test with parameters: \nlength: "+str(length)+"\namount: "+ str(amount)+ "\ninterest: "+str(interest))
     results = calculate(length,amount,interest,desiredResults)
 
-    
+def erroredTests():
+    if failed == "true":
+        print(logFileName)
+        os.rename(logFileName, "failed"+logFileName)
+    else:
+        print("no tests failed")
 
 
 
@@ -58,3 +68,4 @@ log("\n\nStart running test calculate1")
 testCalculation(70,150000,10,calculate1_results) #period in months, amount, interest rate
 log("\n\nStart running test calculate2")
 testCalculation(12,10000,10,calculate2_results)
+erroredTests()
